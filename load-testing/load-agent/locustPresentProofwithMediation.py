e493367c7e2277184089dd642c462b80ac11c344
Alex Walker <alex.walker@indicio.tech>
2023-06-20 11:35:24 -0700

    debugging
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- load-testing/load-agent/locustPresentProofwithMediation.py
+++ load-testing/load-agent/locustPresentProofwithMediation.py
@@ -15,6 +15,12 @@ class CustomLocust(User):
         self.client = CustomClient(self.host)
 
 class UserBehaviour(SequentialTaskSet):
+    def accept_invite(self):
+        self.client.ensure_is_running()
+
+        connection = self.client.accept_invite(self.invite['invitation_url'])
+        self.connection = connection
+
     def get_invite(self):
         invite = self.client.issuer_getinvite()
         self.invite = invite
@@ -22,16 +28,10 @@ class UserBehaviour(SequentialTaskSet):
     def on_start(self):
         self.client.startup(withMediation=True)
         self.get_invite()
+        self.accept_invite()
 
     def on_stop(self):
         self.client.shutdown()
-
-    @task
-    def accept_invite(self):
-        self.client.ensure_is_running()
-
-        connection = self.client.accept_invite(self.invite['invitation_url'])
-        self.connection = connection
 
     @task
     def receive_credential(self):
